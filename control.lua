9a357175e607cd09aa802c49995c7ee010f63ee4
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-06-23 20:17:53 -0700

    save local version of max function

--- control.lua
+++ control.lua
@@ -68,6 +68,7 @@ local speed_thresholds = {
 -- save all these things as local vars so that we don't have to calculate and/or ask the game for them every single time
 local sin = math.sin
 local abs = math.abs
+local max = math.max
 local pi_0 = 0 * math.pi / 3
 local pi_2 = 2 * math.pi / 3
 local pi_4 = 4 * math.pi / 3
