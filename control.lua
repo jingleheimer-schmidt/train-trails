27de423662d98e2072cd8da0c0ae8dcfa1a1cc58
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-07-28 18:04:40 -0700

    fixed that random palette selection sometimes showed only white trails

--- control.lua
+++ control.lua
@@ -36,7 +36,7 @@ local draw_sprite = rendering.draw_sprite
 local function get_random_palette()
   local mod_settings = global.settings
   local palette_name = mod_settings.palette
-  local random_palette_name = random_palette_names[palette_name] and random(#random_palette_names[palette_name]) or nil
+  local random_palette_name = random_palette_names[palette_name] and random_palette_names[palette_name][random(#random_palette_names[palette_name])] or nil
   local random_palette = random_palette_name and animation_palettes[random_palette_name] or nil
   return random_palette
 end
