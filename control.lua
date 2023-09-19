83251a9bcc0f6985ebff59750e0b8087b334db1b
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-09-19 00:38:33 -0700

    yse train_data util

--- control.lua
+++ control.lua
@@ -13,6 +13,10 @@ local trail_types = constants.trail_types
 local active_states = constants.active_states
 local random_palette_names = constants.random_palette_names
 
+local train_data_util = require("util/train_data")
+local add_active_train = train_data_util.add_active_train
+local remove_active_train = train_data_util.remove_active_train
+local reset_active_trains = train_data_util.reset_active_trains
 
 local color_util = require("util/color")
 local get_rainbow_color = color_util.get_rainbow_color
@@ -29,27 +33,6 @@ local pi_2 = 2 * math.pi / 3
 local pi_4 = 4 * math.pi / 3
 local draw_light = rendering.draw_light
 local draw_sprite = rendering.draw_sprite
-
--- add static data to the active_trains table to reduce lookup time
----@param train LuaTrain
-local function add_active_train(train)
-  local random_palette = get_random_palette()
-  global.active_trains[train.id] = {
-    surface_index = train.carriages[1].surface_index,
-    train = train,
-    id = train.id,
-    front_stock = train.front_stock,
-    back_stock = train.back_stock,
-    random_animation_colors = random_palette,
-    random_animation_colors_count = random_palette and #random_palette,
-    adjusted_length = global.settings.length + ((#train.carriages - 1) * 30)
-  }
-end
-
----@param train LuaTrain
-local function remove_active_train(train)
-  global.active_trains[train.id] = nil
-end
 
 -- add new trains to the active_trains table when they are created
 ---@param event EventData.on_train_created
@@ -99,16 +82,6 @@ local function initialize_settings()
     animation_color_count = animation_palettes[palette_name] and #animation_palettes[palette_name],
     palette = palette_name,
   }
-end
-
-local function reset_active_trains()
-  for _, surface in pairs(game.surfaces) do
-    for _, train in pairs(surface.get_trains()) do
-      if active_states[train.state] then
-        add_active_train(train)
-      end
-    end
-  end
 end
 
 local function initialize_and_reset()
---
+++ util/train_data.lua
@@ -0,0 +1,42 @@
+
+local constants = require("util.constants")
+local active_states = constants.active_states
+local color_util = require("util/color")
+local get_random_palette = color_util.get_random_palette
+
+-- add static data to the active_trains table to reduce lookup time
+---@param train LuaTrain
+local function add_active_train(train)
+    local random_palette = get_random_palette()
+    global.active_trains[train.id] = {
+        surface_index = train.carriages[1].surface_index,
+        train = train,
+        id = train.id,
+        front_stock = train.front_stock,
+        back_stock = train.back_stock,
+        random_animation_colors = random_palette,
+        random_animation_colors_count = random_palette and #random_palette,
+        adjusted_length = global.settings.length + ((#train.carriages - 1) * 30)
+    }
+end
+
+---@param train LuaTrain
+local function remove_active_train(train)
+    global.active_trains[train.id] = nil
+end
+
+local function reset_active_trains()
+    for _, surface in pairs(game.surfaces) do
+        for _, train in pairs(surface.get_trains()) do
+            if active_states[train.state] then
+                add_active_train(train)
+            end
+        end
+    end
+end
+
+return {
+    add_active_train = add_active_train,
+    remove_active_train = remove_active_train,
+    reset_active_trains = reset_active_trains
+}
